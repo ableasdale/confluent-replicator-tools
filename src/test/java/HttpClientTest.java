7a4b55b1159ea330f8fa4ae5fbef45beb25dfe46
Alex Bleasdale <ableasdale@confluent.io>
2024-02-26 10:20:00 +0000

    Further work to clean up tests

--- src/test/java/HttpClientTest.java
+++ src/test/java/HttpClientTest.java
@@ -41,23 +41,23 @@ public class HttpClientTest {
         java.net.http.HttpRequest request = java.net.http.HttpRequest.newBuilder()
                 .uri(uri)
                 .header("Accept-Encoding", "gzip")
-                .header("Accept", "application/octet-stream")
+                .header("Accept", ContentType.APPLICATION_OCTET_STREAM.toString())
                 .GET()
                 .build();
         LOG.debug(MessageFormat.format("Request Headers: {0}", request.headers().toString()));
         java.net.http.HttpResponse<String> response = client.send(request, java.net.http.HttpResponse.BodyHandlers.ofString());
         LOG.debug(MessageFormat.format("Response Headers: {0}", response.headers().toString()));
-        assertEquals("gzip",response.headers().firstValue("Content-Encoding").get());
+        assertEquals("gzip", response.headers().firstValue("Content-Encoding").get());
         //assertEquals("application/octet-stream",response.headers().firstValue("Content-Type").get());
-        assertEquals("chunked",response.headers().firstValue("Transfer-Encoding").get());
+        assertEquals("chunked", response.headers().firstValue("Transfer-Encoding").get());
         assertEquals(HTTP_OK, response.statusCode());
 
     }
 
 
-   // @Test
-    public void notSure(){
-       // HttpClient httpClient = HttpClient.create().compress(true).wiretap(true);
+    // @Test
+    public void notSure() {
+        // HttpClient httpClient = HttpClient.create().compress(true).wiretap(true);
 //        HttpClient httpClient = HttpClients.custom()
 //                .setConnectionManager(cm)
 //                .disableContentCompression() //this disables compression
@@ -65,14 +65,14 @@ public class HttpClientTest {
         WebClient client = WebClient.builder()
                 //.compress(true)
                 .baseUrl("http://localhost:9992/files/filename")
-               // .defaultCookie("cookieKey", "cookieValue")
-               // .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .defaultHeader("Content-Type", "application/octet-stream")
-        .defaultHeader("Accept-Encoding","gzip")
-               //.clientConnector(new ReactorClientHttpConnector(client))
+                // .defaultCookie("cookieKey", "cookieValue")
+                // .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader("Content-Type", ContentType.APPLICATION_OCTET_STREAM.toString())
+                .defaultHeader("Accept-Encoding", "gzip")
+                //.clientConnector(new ReactorClientHttpConnector(client))
                 //.defaultUriVariables(Collections.singletonMap("url", "http://localhost:8080"))
                 .build();
-       // WebClient.UriSpec<WebClient.RequestBodySpec> uriSpec = client.get();
+        // WebClient.UriSpec<WebClient.RequestBodySpec> uriSpec = client.get();
 
         /**
          * .get()
@@ -82,63 +82,26 @@ public class HttpClientTest {
          * .bodyToFlux(byte[].class)
          */
     }
+
     // curl -X GET http://localhost:9992/files/f -H "Accept: application/octet-stream" -I
-   // @Test
-    public void aboutToGiveUp() throws IOException {
-        CloseableHttpClient client = HttpClientBuilder.create().build();
-        try {
-            HttpGet post = new HttpGet("http://localhost:9992/files/filename");
 
-
-            //StringEntity entiy = new StringEntity(data, "UTF-8");
-            //post.setEntity(entiy);
-            post.setHeader("Content-Type", "application/octet-stream");
-            post.setHeader("Accept-Encoding","gzip");
-            CloseableHttpResponse response = client.execute(post);
-            for(Header h : response.getHeaders()){
-                LOG.info ("*"+h.toString());
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-   //
-   // @Test
+    @Test
     public void getLargeTxtFileAsChunkedWithDeflateTest() throws IOException, ProtocolException {
-        LOG.info("test");
-        //DecompressingHttpClient
-        CloseableHttpClient client = HttpClientBuilder.create().build();
-        //HttpResponse response = httpClient.execute(new HttpGet("http://localhost:9992/files/filename"));
-               // response -> {
-                    //handle response
-                 //   String bodyAsString = EntityUtils.toString(response.getEntity());
-                    //assertThat(bodyAsString, notNullValue());
 
-
-                   // LOG.info(bodyAsString);
-                  //  return response;
-               // }
-       // );
-//        CloseableHttpClient client = HttpClients.custom()
-//                .disableContentCompression()
-//                .build();
-
+        CloseableHttpClient client = HttpClientBuilder.create().disableContentCompression().build();
         HttpGet request = new HttpGet("http://localhost:9992/files/filename");
         request.setHeader(HttpHeaders.ACCEPT_ENCODING, "deflate");
-        request.addHeader(HttpHeaders.ACCEPT, "application/xml");
-// Header[] headers = response.getHeaders(HttpHeaders.CONTENT_TYPE);
+        request.addHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_OCTET_STREAM.toString());
+
         CloseableHttpResponse response = client.execute(request);//, context);
         //HttpEntity entity = new GzipDecompressingEntity(new ByteArrayEntity(response.getBodyBytes(), ContentType.APPLICATION_OCTET_STREAM));
         //GzipDecompressingEntity entity = (GzipDecompressingEntity) response.getEntity();
-       // Header contentEncodingHeader = entity.getContentEncoding();
-        //assertEquals("deflate", response.getHeader("Content-Encoding"));
-        assertEquals(ContentType.APPLICATION_OCTET_STREAM, response.getHeaders(HttpHeaders.CONTENT_TYPE));
-                //response.getHeader("Content-Type"));
-        assertEquals("chunked", response.getHeader("Transfer-Encoding"));
+        // Header contentEncodingHeader = entity.getContentEncoding();
+        assertEquals("deflate", response.getHeader("Content-Encoding").getValue());
+        assertEquals(ContentType.APPLICATION_OCTET_STREAM.toString(), response.getHeader(HttpHeaders.CONTENT_TYPE).getValue());
+        assertEquals("chunked", response.getHeader("Transfer-Encoding").getValue());
         assertEquals(HTTP_OK, response.getCode());
-        //LOG.info(ContentType.parse(response.getEntity().getContentType()).getMimeType());
-
-
+        response.close();
     }
 
     @Test
@@ -151,49 +114,29 @@ public class HttpClientTest {
         HttpGet request = new HttpGet("http://localhost:9992/files/filename");
         request.setHeader(HttpHeaders.ACCEPT_ENCODING, "gzip");
 
-        HttpResponse response = httpClient.execute(request);
+        CloseableHttpResponse response = httpClient.execute(request);
 
-        for(Header h : response.getHeaders()){
-            LOG.info ("*"+h.toString());
+        for (Header h : response.getHeaders()) {
+            LOG.info("*" + h.toString());
         }
-        assertEquals("gzip",response.getHeader("Content-Encoding").getValue());
-        assertEquals("application/octet-stream",response.getHeader("Content-Type").getValue());
-        assertEquals("chunked",response.getHeader("Transfer-Encoding").getValue());
+        assertEquals("gzip", response.getHeader("Content-Encoding").getValue());
+        assertEquals(ContentType.APPLICATION_OCTET_STREAM.toString(), response.getHeader("Content-Type").getValue());
+        assertEquals("chunked", response.getHeader("Transfer-Encoding").getValue());
         assertEquals(HTTP_OK, response.getCode());
-
+        response.close();
     }
-
 }
-   // public static void main(String[] args) throws IOException {
 
-        /*
-        httpClient.execute(new HttpGet("http://www.google.com"),
-   response -> {
-     assertThat(response.getCode()).isEqualTo(200);
-     return response;
-   }
-);
-
-        httpClient.execute(new HttpGet("http://www.google.com"),
-                response -> {
-                    final String contentMimeType = ContentType.parse(response.getEntity().getContentType()).getMimeType();
-                    assertThat(contentMimeType).isEqualTo(ContentType.TEXT_HTML.getMimeType());
-                    return response;
-                }
-        );
-        httpClient.execute(new HttpGet("http://www.google.com"),
-                response -> {
-                    String bodyAsString = EntityUtils.toString(response.getEntity());
-                    assertThat(bodyAsString, notNullValue());
-                    return response;
-                }
-        );*/
-   // }
-/*
-CloseableHttpClient client = HttpClients.custom()
-    .disableContentCompression()
-    .build();
-
-HttpGet request = new HttpGet(urlSring);
-request.setHeader(HttpHeaders.ACCEPT_ENCODING, "gzip");
+/**
+ * CloseableHttpClient httpclient = HttpClients.createDefault();
+ * HttpGet httpGet = new HttpGet("http://targethost/homepage");
+ * CloseableHttpResponse response1 = httpclient.execute(httpGet);
+ *
+ * try {
+ *     System.out.println(response1.getStatusLine());
+ *     HttpEntity entity1 = response1.getEntity();
+ *     EntityUtils.consume(entity1);
+ * } finally {
+ *     response1.close();
+ * }
  */
